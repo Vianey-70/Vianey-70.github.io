3a82428f0bf5f0e94b09a901b8e1e99d5c5a4557
Vianey-70 <jgutierrezvazquez31@gmail.com>
2024-08-08 18:27:21 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var app = document.getElementById('app');
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+typewriter.typeString('Desarrolladora Front-End Jr.')
+    .pauseFor(2500)
+    .deleteAll()
+    
+    .typeString('¡Si lo puedes imaginar, lo puedes crear!')
+    .pauseFor(2500)
+    .deleteChars(6)
+    .typeString('<strong>programar!</strong>')
+    .pauseFor(2500)
+    .start();
